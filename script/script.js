583166a0261b6596c831934759cf4faac0983b7e
gabrielsc40 <gabrielsimoesc40@gmail.com>
2025-02-26 17:52:22 -0300

    fazendo a responsividade

--- script/script.js
+++ script/script.js
@@ -39,4 +39,21 @@ $(document).ready(function () {
       aboutDevSection.removeClass("visible");
     }
   });
+
+  const tag_name = $(".tag_name").text();
+
+  const array_tag_name = tag_name.split(" ");
+
+  const tag_open = array_tag_name[0];
+  const first_name = array_tag_name[1];
+  const last_name = array_tag_name[2];
+  const tag_close = array_tag_name[3];
+
+  const novo_html = `
+  <span style="color: #00dad2;">${tag_open}</span>
+  ${first_name} ${last_name}
+  <span style="color: #00dad2;">${tag_close}</span>
+`;
+
+$(".tag_name").html(novo_html);
 });
